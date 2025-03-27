64a1123b8de044dda994bd5d1f89558be07795a1
Marie Klisnick <marie63.klisnick@gmail.com>
2025-03-27 17:05:44 +0100

    Ajout du formulaire d'inscription

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,13 +6,13 @@ const NavBar = () => {
       <div className="flex justify-center items-center w-full">
         <ul className="flex space-x-6 text-white">
           <li>
-            <Link to="/" className="hover:bg-blue-500 px-3 py-2 rounded-md text-lg font-medium">Home</Link>
+            <Link to="/" className="hover:bg-indigo-500 px-3 py-2 rounded-md text-lg font-medium">Home</Link>
           </li>
           <li>
-            <Link to="/login" className="hover:bg-blue-500 px-3 py-2 rounded-md text-lg font-medium">Se connecter</Link>
+            <Link to="/login" className="hover:bg-indigo-500 px-3 py-2 rounded-md text-lg font-medium">Se connecter</Link>
           </li>
           <li>
-            <Link to="/register" className="hover:bg-blue-500 px-3 py-2 rounded-md text-lg font-medium">S'inscrire</Link>
+            <Link to="/register" className="hover:bg-indigo-500 px-3 py-2 rounded-md text-lg font-medium">S'inscrire</Link>
           </li>
         </ul>
       </div>
