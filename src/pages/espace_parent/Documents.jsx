4df9b02388813cb9dc3afebb99226c8fafd607c4
Fadia <guelbifedia@gmail.com>
2025-07-07 11:53:57 +0200

    declaration de useNavigate

--- src/pages/espace_parent/Documents.jsx
+++ src/pages/espace_parent/Documents.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../context/authContext";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Documents = () => {
   let navigate = useNavigate();
